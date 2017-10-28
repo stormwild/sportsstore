671a6d1e5505bcccb3b9df22d1ed64163427b61d
Alexander R Torrijos <artorrijos@gmail.com>
2017-10-28 17:07:19 +0800

    Refactored initializer

--- SportsStore/Models/DbInitializer.cs
+++ SportsStore/Models/DbInitializer.cs
@@ -11,24 +11,17 @@ namespace SportsStore.Models
         public static void Initialize(SportStoreContext ctx)
         {
             ctx.Database.EnsureCreated();
-            if(ctx.Products.Any())
-            {
-                return; // Db has been seeded
-            }
 
-            // Seed the data
-            Product[] products = new Product[]
+            if (!ctx.Products.Any())
             {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            };
+                ctx.Products.AddRange(
+                    new Product { Name = "Football", Price = 25 },
+                    new Product { Name = "Surf board", Price = 179 },
+                    new Product { Name = "Running shoes", Price = 95 }
+                );
 
-            foreach(var product in products)
-            {
-                ctx.Products.Add(product);
+                ctx.SaveChanges();
             }
-            ctx.SaveChanges();
         }
     }
 }
